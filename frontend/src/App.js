4b488df5d1090de178f7d2381df03f503910f8ac
OdilovMM <dssrinman@gmail.com>
2024-03-18 05:00:32 +0500

    Feat: Sign in complated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./Routes.js";
-import './App.css'
+import { LoginPage, SignupPage } from "./Routes.js";
+import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,3 +1,4 @@
 import LoginPage from "./pages/Login.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
-export { LoginPage };
+export { LoginPage, SignupPage };
