e7fc8cbfac083989dddcfeeee2940ff410f6ad16
OdilovMM <dssrinman@gmail.com>
2024-03-22 00:06:40 +0500

    Add: storing token to the cookies with useEffect temporarily and later refactor to reduxjs/toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage, SignupPage, ActivationPage } from "./Routes.js";
 import "./App.css";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { server } from "./server.js";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get(`${server}/user/getuser`, { withCredentials: true })
+      .then((res) => {
+        toast.success(res.data.message);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        toast.error(err.response);
+        console.log(err.response);
+      });
+    console.log("render");
+  }, []);
+
+
   return (
     <>
       <BrowserRouter>
